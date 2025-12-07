aae5c7f7b744d29b2a42650071c09cf2ba89bbee
pythonbasiccodes <skshoaib7092@gmail.com>
2025-12-08 00:07:38 +0530

    multiplicationtable

---
+++ multiplicationtable.py
@@ -0,0 +1,6 @@
+x=int(input("enter the number to know table for:"))
+for i in range(1,11):
+    print(x,"x",i,"=",x*i)
+#this gives the right way of table representation 
+# user can give the table number
+# in range function you need to give till 11 because 11 is not included
